463d57aa3e402ec2acdb53c3360e7847143be607
Mantas Mozuraitis <mozmantas@gmail.com>
2023-10-27 00:08:43 +0100

    added responsiveness to the design

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const toggleBtn = $(".toggle_btn");
+const toggleBtnIcon = $(".toggle_btn i");
+const dropDownMenu = $(".dropdown_menu");
 
-
+toggleBtn.on("click", function(){
+    dropDownMenu.toggleClass("open");
+    dropDownMenu.hasClass("open")
+    ?toggleBtnIcon.removeClass("fa fa-fw fa-bars").addClass("fa fa-fw fa-close")
+    :toggleBtnIcon.removeClass("fa fa-fw fa-close").addClass("fa fa-fw fa-bars");
+})
 
