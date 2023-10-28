2771a447d1fdb09c77ce3ec0be9a51930f1895dc
Mantas Mozuraitis <mozmantas@gmail.com>
2023-10-28 22:22:03 +0100

    created aboutus and services pages

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const toggleBtn = $(".toggle_btn");
 const toggleBtnIcon = $(".toggle_btn i");
 const dropDownMenu = $(".dropdown_menu");
 
+const serviceCard = $(".service-card");
+
 toggleBtn.on("click", function(){
     dropDownMenu.toggleClass("open");
     dropDownMenu.hasClass("open")
     ?toggleBtnIcon.removeClass("fa fa-fw fa-bars").addClass("fa fa-fw fa-close")
     :toggleBtnIcon.removeClass("fa fa-fw fa-close").addClass("fa fa-fw fa-bars");
+})
+serviceCard.hover(function(){
+    $(this).find("p").addClass("display-service-desc");
+},
+function(){
+    $(this).find("p").removeClass("display-service-desc");
 })
 
