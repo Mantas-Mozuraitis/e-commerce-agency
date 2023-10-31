2d2396bf784c5efedb638599f66188f9be05cc7b
Mantas Mozuraitis <mozmantas@gmail.com>
2023-10-31 15:52:06 +0000

    Created contacts page

---
+++ form-to-email.php
@@ -0,0 +1,18 @@
+<?php
+   ini_set( 'display_errors', 1 );
+   error_reporting( E_ALL );
+
+   $from = $_POST['user_email'];
+   $to = "getonline@mozuweb.com";
+   $subject = $_POST['user_name'] . "order request";
+   $message = $_POST['user_message'];
+
+   $headers = "MIME-Version: 1.0" . "\r\n";
+   $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+   $headers = "From:" . $from;
+   if(mail($to,$subject,$message, $headers)) {
+      echo '<script>alert("Message was sent successfully");window.location.replace("https://mozuweb.com");</script>'; 
+   } else {
+      echo '<script>alert("Message was not sent");location.reload();</script>'; 
+   }
+?>
